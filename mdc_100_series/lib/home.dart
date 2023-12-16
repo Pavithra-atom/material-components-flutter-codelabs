28708d9f7a2d5e83d0226e69aef139a69ffe63a1
pavithra <pavithra.atomcare@gmail.com>
2023-12-16 11:51:35 +0530

    Add a leading button

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -23,16 +23,28 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
-      
+      appBar: AppBar(
+         leading: IconButton(
+      icon: const Icon(
+        Icons.menu,
+        semanticLabel: 'menu',
+      ),
+      onPressed: () {
+        print('Menu button');
+      },
+    ),
+            title: const Text('SHRINE'),
+            
+      ),
+        
       // TODO: Add a grid view (102)
        body: Center(
         child: Text('You did it!'),
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
-            resizeToAvoidBottomInset: false,
-
+      
     );
   }
 }
